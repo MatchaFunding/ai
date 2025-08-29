3c6c194745d34f9d453a84dbf26ef7f993ee3e2a
Monki <barahona.nico@gmail.com>
2025-08-28 21:37:00 -0400

    Se crea el modelo idea_processed.py

---
+++ app/models/idea-processed.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class IdeaProcessed(BaseModel):
+    ID: int = Field(...)
+    Usuario: int = Field(...)
+    Campo: str
+    Problema: str
+    Publico: str
+    Innovacion: str
+    Embedding: List[float] = Field(..., description="Vector numérico generado para matching")
